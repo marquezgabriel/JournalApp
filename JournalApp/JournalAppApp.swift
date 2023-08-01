63c6efe5c3e8b34b6377ca76e2226f6cb0ed4d8b
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-08-01 16:42:46 -0400

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Gabriel Marquez on 2023-08-01.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
